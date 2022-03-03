394b7b3b8c049b9a6efcc2796c40a742dc1a72a7
giacoletti <giacoletti92@gmail.com>
2022-03-03 22:25:22 +0100

    Install mui styling packages add Roboto google font to index.html add basic mui containers in main view

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Container, Paper, List, ListItem, Typography } from "@mui/material";
 
 const App = () => {
-  return <h1 data-cy="main-header">To-Do List</h1>;
+  return (
+    <Container>
+      <Typography
+        data-cy="main-header"
+        variant="h3"
+        component="div"
+        gutterBottom
+      >
+        To-Do List
+      </Typography>
+      <Paper sx={{ maxWidth: 345, boxShadow: 3 }}>
+        <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+          <ListItem>Test</ListItem>
+        </List>
+      </Paper>
+    </Container>
+  );
 };
 
 export default App;
